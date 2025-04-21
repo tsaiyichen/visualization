016d20daf16e536ca03bb8eb0c5ec6ea1c9e6bd4
tsaiyichen <easonjessiejj@gmail.com>
2025-04-21 16:24:24 +0800

    2024party

--- 2024_party_pieChart.py
+++ 2024_party_pieChart.py
@@ -16,7 +16,18 @@ color_dic = {
 
 #load df
 df = pd.read_csv('data/2024_party_clean.csv')
-df.set_index('number', inplace=True)
-df['color'] = df['party'].map(color_dic)
+df.set_index('name', inplace=True)
+df['color'] = df.index.map(color_dic)
 
-print(df)
+#create fig
+fig, ax = plt.subplots(figsize=(8, 6.4))
+
+ax = ax.pie(x = df['percentage'], colors=df['color'], labels=df.index, autopct='%.0f%%')
+
+plt.title('2024年全國不分區立委選舉結果')
+
+plt.xlabel('得票比例（獲得席次門檻：5%）')
+plt.text(1.2, 0.85, '席次：\n民主進步黨：13\n中國國民黨：13\n台灣民眾黨：8',backgroundcolor = 'white', fontsize=12, color = 'black')
+fig.patch.set_facecolor('#E5C2A9')
+
+plt.savefig('image/2024_party_pieChart.png', dpi=300, bbox_inches='tight')
--- 2024_party_preprocessing.py
+++ 2024_party_preprocessing.py
@@ -4,10 +4,16 @@ df = pd.read_csv('data/2024_party.csv')
 
 df.columns = ['name', 'votes', 'percentage', 'threshold']
 df.drop('threshold', axis=1, inplace=True)
-df.drop(len(df) - 1, axis=0, inplace=True)
+#df.drop(len(df) - 1, axis=0, inplace=True)
 df.set_index('name', inplace=True)
+
 df = df[df['percentage'] > 3]
 
-df.loc['其他'] = 100 - df['percentage'].sum()
+df['votes'] = df['votes'].str.replace(',', '', regex=False)
+df['votes'] = df['votes'].astype(int)
+
+df.loc['其他'] = [df.loc['合計']['votes'] - df.head(len(df)-1)['votes'].sum(), 100 - df.head(len(df)-1)['percentage'].sum()]
+
+df.drop('合計', axis=0, inplace=True)
 
 df.to_csv('data/2024_party_clean.csv', encoding='utf-8-sig')
