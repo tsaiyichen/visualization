80b006e17bd866644740f92853b99a3172c3300f
tsaiyichen <easonjessiejj@gmail.com>
2025-04-22 02:08:33 +0800

    2024 region detail data preprocessing finish

---
+++ 2024_presidient_detail_preprocessing.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import geopandas as gpd
+region_dictionary = {
+    '臺北市': "Taipei City",
+    '新北市': "New Taipei City",
+    '基隆市': "Keelung City",
+    '桃園市': 'Taoyuan City',
+    '新竹縣': 'Hsinchu',
+    '新竹市': 'Hsinchu city',
+    '苗栗縣': 'Miaoli',
+    '臺中市': 'Taichung City',
+    '彰化縣': "Changhua",
+    '南投縣': 'Nantou',
+    '雲林縣': 'Yunlin',
+    '嘉義縣': 'Chiayi',
+    '嘉義市': 'Chiayi City',
+    '臺南市': 'Tainan City',
+    '高雄市': 'Kaohsiung City',
+    '屏東縣': 'Pingtung',
+    '宜蘭縣': 'Yilan',
+    '花蓮縣': 'Hualien',
+    '臺東縣': 'Taitung',
+    '澎湖縣': 'Penghu',
+    '金門縣': 'Kinmen',
+    '連江縣': 'Matsu Islands'
+}
+candidates_dictionary = {
+    1: ('柯文哲、吳欣盈', '台灣民眾黨'),
+    2: ('賴清德、蕭美琴', '民主進步黨'),
+    3: ('侯友宜、趙少康', '中國國民黨')
+}
+df = pd.read_csv('data/2024_president_detail.csv')
+df.dropna(axis=0, inplace=True)
+df.drop(df.columns[5:], axis=1, inplace=True)
+df.set_index('行政區別', inplace=True)
+df.index = df.index.str.strip()
+df.columns = df.columns.str.replace('\n', ' ', regex=False)
+df.columns = [1, 2, 3, 'total']
+df = df.replace(',', '', regex=True)
+df = df.astype(int)
+df['name'] = df.index.map(region_dictionary)
+
+gdf = gpd.read_file('data/tw.json')
+df = gdf.merge(df, on='name')
+df.set_index('name', inplace=True)
+
+df['winner_number'] = df[[1, 2, 3]].idxmax(axis=1)
+
+print(df)
+df.to_csv('data/2024_president_detail_clean.csv', encoding='utf-8-sig')
