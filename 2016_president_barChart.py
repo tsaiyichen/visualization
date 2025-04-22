147b7d4fdfd030da43cfac2acfcd1b9ead587b15
tsaiyichen <easonjessiejj@gmail.com>
2025-04-22 21:38:15 +0800

    finish all barChart

---
+++ 2016_president_barChart.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import matplotlib.ticker as ticker
+plt.rcParams['font.family'] = 'Microsoft JhengHei'
+
+#set color dictionary
+color_dic = {
+    '中國國民黨推薦': 'blue',
+    '民主進步黨推薦': 'green',
+    '親民黨推薦': 'orange',
+    '台灣民眾黨推薦': 'aqua',
+    '無黨籍': 'gray'
+}
+
+#load df
+df = pd.read_csv('data/2016_president_clean.csv', index_col=0)
+df['color'] = df['party'].map(color_dic)
+df.dropna(axis=0, inplace=True)
+
+#create a figure
+fig, ax = plt.subplots(figsize=(12, 6))
+
+#setting the bar chart and its label
+bar = ax.bar(x=df.index, height=df['votes'], color=df['color'], width=0.5)
+ax.bar_label(container= bar, labels=df['votes'], padding=0, color='black', label_type='center')
+df['percentage'] = ((df['percentage']*100).round(2)).astype(str) + '%'
+ax.bar_label(container= bar, labels=df['percentage'], padding=-2, color='black', label_type='edge')
+print(df)
+
+#setting the title, xlabel, ylabel
+plt.title('2016年總統副總統選舉結果')
+plt.xlabel('候選人')
+plt.ylabel('得票數（單位：萬）')
+def to_ten_thousand(x, pos):
+    return f'{x / 10000:.0f}'
+formatter = ticker.FuncFormatter(to_ten_thousand)
+ax.yaxis.set_major_formatter(formatter) #y-axis scale
+
+#setting the color of the background
+fig.patch.set_facecolor('#E5C2A9')
+ax.patch.set_facecolor('#E5C2A9')
+
+plt.savefig('image/2016_president_barChart.png', dpi=300, bbox_inches='tight')
+plt.show()
---
+++ 2020_president_preprocessing.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+df = pd.read_csv('data/2020_president.csv', index_col=0)
+
+df.dropna(axis=0, inplace=True)
+df = df.T
+df.columns = ['votes', 'party']
+df['votes'] = df['votes'].str.replace(',', '', regex=False)
+df['votes'] = df['votes'].astype(int)
+df['percentage'] = (df['votes'] / df.loc['total']['votes']).round(4)
+print(df)
+df.to_csv('data/2020_president_clean.csv', encoding='utf-8-sig')
